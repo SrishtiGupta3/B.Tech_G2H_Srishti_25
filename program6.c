193120923c4a37d0767cab42f286b1e3daead952
SrishtiGupta3 <gsrishti252@gmail.com>
2026-02-13 11:33:49 +0530

    Add files via upload

---
+++ program6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+printf("Enter two numbers \n");
+scanf("%d %d",&a,&b);
+if(a==b){
+  printf("\nThe numbers are  equal.");  
+}
+else 
+{
+    printf("\nThe numbers are not equal.");
+}  return 0;
+}
