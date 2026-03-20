525aacea55d09e837d254a079f299d811fd8f2cf
SrishtiGupta3 <gsrishti252@gmail.com>
2026-03-20 10:26:25 +0530

    Add files via upload

---
+++ sumarray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i,n,sum;
+    printf("enter the size of an array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the elemnts of array:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+}
+printf("\n");
+for(i=0;i<n;i++){
+        printf("arr[%d] : %d \n",i,arr[i]);
+}
+printf("\n");
+sum=0;
+for(i=0;i<n;i++){
+    sum=sum+arr[i];
+}
+printf("The sum of elements are %d",sum);
+return 0;
+}
