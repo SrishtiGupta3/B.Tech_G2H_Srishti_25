0a035b248e7c3b8348c1b588c461ac1abfbdcc7f
SrishtiGupta3 <gsrishti252@gmail.com>
2026-03-20 10:17:30 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -10,12 +10,12 @@ int main(){
 }
 printf("\n");
 for(i=0;i<n;i++){
-        printf("%d",arr[i]);
+        printf("arr[%d] : %d \n",i,arr[i]);
 }
 printf("\n");
 printf("reverse array is :\n");
 for(i=(n-1);i>=0;i--){
-     printf("%d",arr[i]);
+     printf("arr[%d] : %d \n",i,arr[i]);
 }
 return 0;
 }
