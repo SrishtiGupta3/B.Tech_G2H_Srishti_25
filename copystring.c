3b54dded9f681294dc01746047e52a27f8b14a10
SrishtiGupta3 <gsrishti252@gmail.com>
2026-03-27 11:26:54 +0530

    Add files via upload

---
+++ copystring.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int N,N2;
+    printf("Enter the value of N:");
+    scanf("%d",&N);
+    printf("Enter the value of N2:");
+    scanf("%d",&N2);
+    getchar();
+    char a[N];
+    char b[N2];
+    printf("Enter string :");
+gets(a);
+printf("The 1st string is:%s \n",a);
+strcpy(b,a);
+printf("\nThe copied string(2nd) from 1st is: %s \n",b);
+return 0;
+}
