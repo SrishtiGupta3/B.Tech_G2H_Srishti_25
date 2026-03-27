d64f42ca693451e8522b93b8adf4ce3dd3f410f2
SrishtiGupta3 <gsrishti252@gmail.com>
2026-03-27 10:17:03 +0530

    Add files via upload

---
+++ series.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int N,i;
+    float sum;
+    printf("Enter the value of number:");
+    scanf("%d",&N);
+    sum=0.0;
+    for(i=1;i<=N;i++){
+    sum=sum+1.0/i;}
+printf("sum of series is :%f",sum);
+return 0;
+}
