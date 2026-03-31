2cce159182a973bf06e74b0bf30a3e08906c5fa5
SrishtiGupta3 <gsrishti252@gmail.com>
2026-03-31 16:29:25 +0530

    Add files via upload

---
+++ reversestring.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int i,n,length;
+    char str[100];
+    printf("Enter your string:");
+//  gets(str);
+ fgets(str,100,stdin);
+ for(i=0;str[i];i++){
+ n=n+1;
+ }
+length = n-1;
+    for(i=length;i>=0;i--){
+      printf("%c",str[i]);
+    } 
+   //  puts(str);
+  return 0;
+   }
